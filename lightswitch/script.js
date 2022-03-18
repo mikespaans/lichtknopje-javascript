ed769436e7a022a7c10d4d96a89efcc2709c5155
Mike Spaans <99069233@mydavinci.nl>
2022-03-18 11:24:35 +0100

    javascript lichtknop afgemaakt

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -6,5 +6,23 @@ document.body.appendChild(btn);
 // schijf hier tussen je code
 
 
+function KnopAan_Uit(){
+    if (btn.innerHTML == "switch light off" ) {;
+        btn.innerHTML = 'switch light on';
+        console.log("light is off")
+        document.body.style.backgroundColor = "black";
+
+
+   }else {
+        btn.innerHTML = "switch light off";
+        console.log("light is on")
+        document.body.style.backgroundColor = "yellow";
+    }}
+
+
+document.body.style.backgroundColor = "black";
+btn.innerHTML = "switch light on"
+btn.onclick = KnopAan_Uit
+
 
 // schijf hier tussen je code
---
+++ lightswitch/test.js
@@ -0,0 +1,12 @@
+{/* <button class="btn success">Success</button> */}
+
+
+function PrintLog(){
+    if (btn.innerHTML == "switch light off ") {;
+        btn.innerHTML = "switch light on";
+        console.log = "light is off";
+    }
+    else {
+        btn.innerHTML = "switch light on";
+        console.log = "light is off"
+    }
